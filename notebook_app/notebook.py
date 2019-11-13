2d62c09355f288a1f6494785deb1e7100573d350
wangonya <kwangonya@gmail.com>
2019-11-13 21:14:05 +0300

    add NoteBook class

--- notebook_app/notebook.py
+++ notebook_app/notebook.py
@@ -19,6 +19,9 @@ class Note:
         last_id += 1
         self.id = last_id
 
+    def __repr__(self):
+        return f"{self.id} - {self.memo}"
+
     def match(self, filter):
         """Determine if this note matches the filter
         text. Return True if it matches, False otherwise.
@@ -27,3 +30,36 @@ class Note:
         return filter in self.memo or filter in self.tags
 
 
+class NoteBook:
+    """Represent a collection of notes that can be tagged,
+    modified and searched"""
+
+    def __init__(self):
+        """Initialize notebook with an empty list"""
+        self.notes = []
+
+    def new_note(self, memo, tags=''):
+        """Create a new note and add it to the list"""
+        self.notes.append(Note(memo, tags))
+
+    def update_note(self, note_id, memo='', tags=''):
+        """Find memo with the given id and update it"""
+        for note in self.notes:
+            if note.id == note_id:
+                if memo:
+                    note.memo = memo
+                if tags:
+                    note.tags = tags
+                break
+
+    def list_all(self):
+        """
+        List all saved notes
+        :return: all notes in self.notes[]
+        :rtype: list
+        """
+        return self.notes
+
+    def search(self, filter):
+        """Find all notes that match the given filter string"""
+        return [note for note in self.notes if note.match(filter)]
